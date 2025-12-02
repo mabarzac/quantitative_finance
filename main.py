5015d91ae120156bfae677b961b5ad1eaf8513f6
mabarzac <mabarzac@gmail.com>
2025-12-02 17:13:45 +0000

    rama actualizada

--- main.py
+++ main.py
@@ -1,32 +1,60 @@
+import pandas as pd
 import numpy as np
 from modules.financials_functions import portafolio_volatility, portfolio_returns, VaR
+from modules.backend import tickers_by_issuer
 
 
 if __name__ == '__main__': 
-    #datos del portafolio
-    tickers = ["IEF", "SPTL", "TLT", "VGLT"]
-    start = "2023-01-01"
-    end = "2024-12-31"
+   
+   # obtener tickers de iShares
+   tickers = tickers_by_issuer (issuer ='iShares')
 
-    #descargar retornos del portafolio 
-    df = portfolio_returns(
-        tickers=tickers,
-        start=start,
-        end=end
+   #portafolio de renta fija 
+   tickers_rf = tickers[tickers['CATEGORIA']=='ETF RF']
+   list_tickers_rf = list(tickers_rf['TICKER'])
+
+   #portafolio de renta variable
+   tickers_rv = tickers[tickers['CATEGORIA']=='ETF RV']
+   list_tickers_rv = list(tickers_rv['TICKER'])
+
+   # rango de fechas
+   start = '2024-01-01'
+   end = '2024-12-31'
+
+   # nivel de confianza
+   confidence = 0.05
+   lst = []
+
+for portafolio in [list_tickers_rf, list_tickers_rv]:
+        
+        #Obtener retornos de portafolio
+        df = portfolio_returns(tickers=portafolio, start=start, end=end)
+        
+
+        vector_w =np.array( [1/len(portafolio)] * len(portafolio)) 
+        
+        #calcular volatilidad
+        sigma = portafolio_volatility(df=df,vector_w=vector_w)
+       
+
+        # Calcular VaR
+        var= VaR(sigma=sigma, confidence=confidence)
+        var= np.abs(var)
+        var_mensual = var* np.sqrt(20)
+        lst.append(var_mensual)
+
+df_final = pd.DataFrame(
+        {
+        'PORTAFOLIO':['Ishares Renta Fija','Ishares Renta Variable'],
+        f'Value at Risk:{1-confidence}%':lst
+        }
         )
-    print(df.head(5))
 
-    #calculo volatilidad
-    vector_w=np.array([1/len(tickers)]*len(tickers))
-    sigma = portafolio_volatility(df=df,vector_w=vector_w)
-    print(sigma)
+df_final = df_final.sort_values(
+        by=f'Value at Risk:{1-confidence}%',
+        ascending = False
+        )
+print(df_final)
+   
+        
 
-    print('='*100)
-    confidence =0.05
-    var = VaR(sigma=sigma,confidence=confidence)
-    print(var)
-
-    
-
-
-    
